a7b0b91f120081e91254a9eb0b1cfc9232584553
Ishuklaji <ishuklaji100@gmail.com>
2023-06-24 01:53:17 +0530

    working

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,32 +1,73 @@
-import Container from 'react-bootstrap/Container';
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import React, { useEffect } from "react";
+import {
+    Container,
+    Form,
+    FormControl,
+    Nav,
+    Navbar,
+    NavDropdown,
+} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { } from "react-router-dom";
+import { logout } from "../actions/userActions";
 
-const Header = () => {
+function Header({ setSearch }) {
+    const dispatch = useDispatch();
+
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userInfo } = userLogin;
+
+    const logoutHandler = () => {
+        dispatch(logout());
+    };
+
+    useEffect(() => { }, [userInfo]);
+
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                <Navbar.Brand href="/">My Note Zipper</Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="m-auto">
+                        {userInfo && (
+                            <Form inline>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Search"
+                                    className="mr-sm-2"
+                                    onChange={(e) => setSearch(e.target.value)}
+                                />
+                            </Form>
+                        )}
+                    </Nav>
+                    <Nav>
+                        {userInfo ? (
+                            <>
+                                <Nav.Link href="/mynotes">My Notes</Nav.Link>
+                                <NavDropdown
+                                    title={`${userInfo.name}`}
+                                    id="collasible-nav-dropdown"
+                                >
+                                    <NavDropdown.Item href="/profile">
+                                        My Profile
+                                    </NavDropdown.Item>
+
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item onClick={logoutHandler}>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            </>
+                        ) : (
+                            <Nav.Link href="/login">Login</Nav.Link>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-    )
+    );
 }
 
-export default Header
+export default Header;
