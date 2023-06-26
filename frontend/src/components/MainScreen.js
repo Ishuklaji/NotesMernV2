6280adfdd231a0ad8d8addb57d22b9d40d2e6beb
Ishuklaji <ishuklaji100@gmail.com>
2023-06-27 01:53:10 +0530

    working

--- frontend/src/components/MainScreen.js
+++ frontend/src/components/MainScreen.js
@@ -2,12 +2,22 @@ import {
     Container,
     Row, Col
 } from "react-bootstrap";
+import "./MainScreen.css"
 
-const MainScreen = () => {
+const MainScreen = ({ title, children }) => {
     return (
         <div className='mainback'>
             <Container>
-
+                <Row>
+                    <div className='page'>
+                        {title && (<>
+                            <h1 className='heading'>{title}</h1>
+                            <hr />
+                        </>
+                        )}
+                        {children}
+                    </div>
+                </Row>
             </Container>
         </div>
     )
--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,8 +1,17 @@
+import MainScreen from "../../components/MainScreen"
+import { Link } from "react-router-dom"
+import {
+    Button
+} from "react-bootstrap";
 const MyNotes = () => {
     return (
-        <h1>
-            My Notes
-        </h1>
+        <MainScreen title='Welcome '>
+            <Link to='createnote'>
+                <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
+                    Create New Note
+                </Button>
+            </Link>
+        </MainScreen>
     )
 }
 
