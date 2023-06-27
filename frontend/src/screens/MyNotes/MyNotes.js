ba07c5ce24411ae92295c9ea9e0f6e1ec0609551
Ishuklaji <ishuklaji100@gmail.com>
2023-06-28 02:09:27 +0530

    working

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,15 +1,43 @@
 import MainScreen from "../../components/MainScreen"
 import { Link } from "react-router-dom"
 import {
-    Button
+    Button, Card
 } from "react-bootstrap";
+import notes from "../../data/notes"
 const MyNotes = () => {
+    const deleteHandler = (id) => {
+        if (window.confirm("Are you sure ?")) {
+
+        }
+    }
     return (
         <MainScreen title='Welcome '>
             <Link to='createnote'>
                 <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
                     Create New Note
                 </Button>
+                {
+                    notes.map(note => (
+                        <Card style={{ margin: 10 }}>
+                            <Card.Header style={{ display: 'flex' }}>
+                                <span
+                                    style={{
+                                        color: "black",
+                                        textDecoration: "none",
+                                        flex: 1,
+                                        cursor: "pointer",
+                                        alignSelf: "center",
+                                        fontSize: 18
+                                    }}>{note.title}</span>
+                                <div>
+                                    <Button href={`/note/${note._id}`}>Edit</Button>
+                                    <Button variant='danger' className='mx-2' onClick={() => deleteHandler(note._id)}>Delete</Button>
+                                </div>
+                            </Card.Header>
+                        </Card>
+                    ))
+                }
+
             </Link>
         </MainScreen>
     )
