2450a935dabf945a820329bc4c142e6a4f489842
Ishuklaji <ishuklaji100@gmail.com>
2023-06-29 02:11:51 +0530

    working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} exact/>
           <Route path="/mynotes" element={<MyNotes />} />
-          <Route path="/contact" element={<LandingPage />} />
+          <Route path="/contac" element={<LandingPage />} />
         </Routes> 
       </main>
       <Footer />
