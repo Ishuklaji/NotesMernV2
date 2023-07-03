7ae64af46d04e389a2d46f895496ca227168c01f
Ishuklaji <ishuklaji100@gmail.com>
2023-07-04 02:03:04 +0530

    router creation

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const notes = require('./data/notes')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 dotenv.config()
@@ -15,10 +16,7 @@ app.get('/api/notes', (req, res) => {
     res.json(notes)
 })
 
-app.get('/api/notes/:id', (req, res) => {
-    const note = notes.find((n) => n._id === req.params.id)
-    res.send(note)
-})
+app.use('/api/users',userRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server started at http://localhost:${PORT}`));
