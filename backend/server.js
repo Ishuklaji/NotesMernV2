a2812aa9deceb7343bc25421d67787284c8a0285
Ishuklaji <ishuklaji100@gmail.com>
2023-08-16 00:51:38 +0530

    updated jwt bcrypt

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ const notes = require('./data/notes')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const userRoutes = require('./routes/userRoutes')
+const {notFound,errorHandler} = require("./middlewares/errorMiddleware")
 
 const app = express()
 dotenv.config()
 connectDB()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send("API server is running...")
@@ -17,6 +19,9 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.use('/api/users',userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server started at http://localhost:${PORT}`));
