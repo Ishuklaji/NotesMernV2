76c58f55a376e56be7c6fa819a6d4bf7fa211277
Ishuklaji <ishuklaji100@gmail.com>
2023-08-17 02:23:27 +0530

    login register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 // import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import LandingPage from "./screens/LandingPage/LandingPage"
+import LoginScreen from "./screens/LoginScreen/LoginScreen"
+import RegisterScreen from "./screens/RegisterScreen/RegisterScreen"
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import MyNotes from "./screens/MyNotes/MyNotes"
 function App() {
@@ -12,7 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} exact/>
           <Route path="/mynotes" element={<MyNotes />} />
-          <Route path="/contac" element={<LandingPage />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/register" element={<RegisterScreen />} />
         </Routes> 
       </main>
       <Footer />
